59b8d4e707135d8082135b4a24b61d355565380a
tufeeqmushtaqnahami <tufeeqmushtaq000@gmail.com>
2025-09-15 23:40:11 +0530

    Added Script and CSS to the Task List  function

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
-document.addEventListener('DOMContentLoaded',()=>{
-   const taskInput = document.getElementById('task-input');
-   const addTaskBtn = document.getElementById('add-task-btn');
-   const taskList = document.getElementById('task-list');
-   const emptyImage = document.getElementById('empty-image');
+document.addEventListener('DOMContentLoaded', () => {
+  const taskInput = document.getElementById('task-input');
+  const addTaskBtn = document.getElementById('add-task-btn');
+  const taskList = document.getElementById('task-list');
+  const emptyImage = document.querySelector('.empty-image');
+   
 
 
 
-   const toggleEmptyState = ()=>{
-    emptyImage.style.display 
 
-   }
+  const toggleEmptyState = () => {
+    emptyImage.style.display = taskList.children.length === 0 ? 'block' : 'none';
+
+  };
 
 
-   const addTask = (event)=>{
+  const addTask = (event) => {
     event.preventDefault();
     const taskText = taskInput.value.trim();
-    if(!taskText){
+    if (!taskText) {
       return;
     }
     const li = document.createElement('li');
-    li.textContent = taskText;
+     li.innerHTML = `
+     <input type="checkbox"  class="checkbox">
+     <span>${taskText}</span>`;
+
+
+
     taskList.appendChild(li);
     taskInput.value = '';
-   }
+    toggleEmptyState();
+  }
 
 
-   addTaskBtn.addEventListener('click', addTask);
-   taskInput.addEventListener('keypress', (e)=>{
-    if (e.key === 'Enter'){
+  addTaskBtn.addEventListener('click', addTask);
+  taskInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
       addTask(e);
     }
-   })
+  })
 })
+
+
+
