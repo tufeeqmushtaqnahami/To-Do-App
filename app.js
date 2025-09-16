45162121a8cb7d0ea7a2a7c9ea28a131c3fe7c0c
tufeeqmushtaqnahami <tufeeqmushtaq000@gmail.com>
2025-09-16 23:03:57 +0530

    Added the progress bar section with CSS , Made the Edit and Remove button interactive

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const todosContainer = document.querySelector('.todos-container')
 
 
-   
+
 
 
 
@@ -18,36 +18,78 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
 
-  const addTask = (event) => {
-    event.preventDefault();
-    const taskText = taskInput.value.trim();
+  const addTask = (text, completed = false) => {
+
+    const taskText = text || taskInput.value.trim();
     if (!taskText) {
       return;
     }
     const li = document.createElement('li');
-     li.innerHTML = `
-     <input type="checkbox"  class="checkbox">
+    li.innerHTML = `
+     <input type="checkbox"  class="checkbox"${completed ? 'checked' : ''}/>
      <span>${taskText}</span>
      <div 
           class="task-buttons">
           <button class="edit-btn"><i class="fa-solid fa-pen"></i></button>
           <button class="delete-btn"><i class="fa-solid fa-trash"></i></button>
      </div>
-     `
-     
+     `;
+
+
+    const checkbox = li.querySelector('.checkbox');
+    const editBtn = li.querySelector('.edit-btn');
+
+    if (completed) {
+      li.classList.add('completed');
+      editBtn.disabled = true;
+      editBtn.style.opacity = '0.5'
+      editBtn.style.pointerEvents = 'none'
+    }
+
+
+    checkbox.addEventListener('change', () => {
+      const isChecked = checkbox.checked;
+      li.classList.toggle('completed', isChecked);
+      editBtn.disabled = isChecked;
+      editBtn.style.opacity = isChecked ? '0.5' : '1';
+      editBtn.style.pointerEvents = isChecked ? 'none' : 'auto';
+    })
+
+
+
+
+    editBtn.addEventListener('click', () => {
+      if (!checkbox.checked) {
+        taskInput.value = li.querySelector('span').textContent;
+        li.remove();
+        toggleEmptyState();
+      }
+
+    })
+
+    li.querySelector('.delete-btn').addEventListener('click', () => {
+      li.remove();
+      toggleEmptyState()
+    })
+
 
 
 
     taskList.appendChild(li);
     taskInput.value = '';
     toggleEmptyState();
-  }
+  };
 
 
-  addTaskBtn.addEventListener('click', addTask);
+  addTaskBtn.addEventListener('click', () => {
+    addTask();
+  });
+
+
   taskInput.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
-      addTask(e);
+      e.preventDefault();
+      addTask();
     }
   })
 })
